350cbe9f17cfcca5de29f7c682348b1464d4fb98
Soufian Tichattibin <Soufian.Tichattibin@infosupport.com>
2020-04-20 12:19:01 +0200

    first commit (hopefully last)

---
+++ config-app/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace config_app.Controllers
+{
+    [Route("apple-app-site-association")]
+    public class HomeController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return Json(
+                new {
+                    applinks = new
+                    {
+                        apps = new List<string>(),
+                        details = new List<object> 
+                        {
+                            new
+                            {
+                                appID = "BDCC64DUG6.infosupport.soufiant.Info-Card",
+                                paths= new List<string> { "*" }
+                            }
+                        }
+                    }
+            }
+            );
+        }
+    }
+}
