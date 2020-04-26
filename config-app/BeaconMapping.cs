88cf9aca06615c9162784840ee62d955022c3302
Soufian Tichattibin <Soufian.Tichattibin@infosupport.com>
2020-04-26 12:17:43 +0200

    add beaconmapping dummy

---
+++ config-app/BeaconMapping.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace config_app
+{
+    public class BeaconMapping
+    {
+        public string ConnectableName { get; set; }
+        public string ProximityUuid { get; set; }
+        public string Identifier { get; set; }
+    }
+}
---
+++ config-app/Controllers/BeaconMappingController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace config_app.Controllers
+{
+    [ApiController]
+    [Route("beacon-mapping")]
+    public class BeaconMappingController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<BeaconMapping> Get()
+        {
+            return new BeaconMapping[] 
+            { 
+                new BeaconMapping {
+                    ConnectableName="HMSoft",
+                    ProximityUuid="644f76f7-6a52-42bc-e911-fd902c9bb987",
+                    Identifier="",
+                }
+            };
+        }
+    }
+}
